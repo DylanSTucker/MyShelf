462c5b140e338491146e561f19135d15c7e87751
Dylan Tucker <66965464+DSTuckster@users.noreply.github.com>
2024-05-02 10:14:29 -0600

    created a simple express server

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginNode } from "vite-plugin-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 8080,
+  },
+  plugins:[
+    react(),
+    ...VitePluginNode({
+      adapter: "express",
+      appPath: "./src/server.ts",
+      exportName: "MyShelf",
+      tsCompiler: "esbuild",
+      swcOptions: {},
+    }),
+  ],
+});
