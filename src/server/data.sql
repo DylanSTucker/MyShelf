68254b810e9231db9bc62970ed9cc66e170ff430
Dylan Tucker <66965464+DSTuckster@users.noreply.github.com>
2024-05-02 21:03:37 -0600

    added some server code and docker files for postgres database

---
+++ src/server/data.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE myshelf;
+
+CREATE TABLE shelf(
+    id VARCHAR(255) PRIMARY KEY,
+    book_title VARCHAR(100),
+    book_author VARCHAR(100),
+    book_publisher VARCHAR(100)
+);
+
+CREATE TABLE users(
+    email VARCHAR(255) PRIMARY KEY,
+    hashed_password VARCHAR(255)
+);
